275cc21e92e7c9265e2017757f913c7e9320b87e
kotokkk <ketringggold@mail.ru>
2022-05-17 15:57:49 +0300

    add app.py and 1.txt

---
+++ app.py
@@ -0,0 +1,41 @@
+name = "Ivan"
+print(name, type(name))
+age = 24
+print(age, type(age))
+room_area = 3.45
+print(room_area, type(room_area))
+a=b"Python"
+print(a, type(a))
+sistersNames = ["Anna", "Julia"]
+print(sistersNames, type(sistersNames))
+data = (9, "London", 3.67, 0)
+print(data, type(data))
+ThisSet = {'w2', 'w2', 'q1','f'}
+ThisSet.remove('q1')
+print(ThisSet, type(ThisSet))
+this_frozen_set = frozenset('cat')
+print(this_frozen_set, type(this_frozen_set))
+dict_sample = {"Season": "Spring", "Year": 2022}
+print(dict_sample, type(dict_sample))
+
+first_name = "Ivan "
+last_name = "Pupkin"
+fl = first_name + last_name
+print(fl)
+
+a = 36
+b = 5
+print(a + b)
+c = a / b
+print(c)
+f = a * b
+print(f)
+d = a // b
+print(d)
+s = a % b
+print(s)
+qqt = ((7+12)**3+7*4-44/2**4)
+print(qqt)
+
+
+
